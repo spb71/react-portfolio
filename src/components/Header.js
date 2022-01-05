006ec965c3970495dad81de2967fbf5626dd60f9
spb71 <shem.baijoo@gmail.com>
2022-01-04 21:31:54 -0500

    Added react-bootstrap module and updated projects content with appropriate projects, images, and repo links

--- src/components/Header.js
+++ src/components/Header.js
@@ -1,44 +1,23 @@
 import React from "react";
+import { Nav } from "react-bootstrap";
+import { Navbar } from "react-bootstrap";
+import { Container } from "react-bootstrap";
 
 const Header = () => {
   return (
-    <nav className="navbar navbar-expand-lg navbar-dark bg-dark">
-      <div className="container-fluid">
-        <a className="navbar-brand" href="#">
-          Shem Baijoo
-        </a>
-        <button
-          className="navbar-toggler"
-          type="button"
-          data-bs-toggle="collapse"
-          data-bs-target="#navbarSupportedContent"
-          aria-controls="navbarSupportedContent"
-          aria-expanded="false"
-          aria-label="Toggle navigation"
-        >
-          <span className="navbar-toggler-icon"></span>
-        </button>
-        <div className="collapse navbar-collapse" id="navbarSupportedContent">
-          <ul className="navbar-nav me-auto mb-2 mb-lg-0">
-            <li className="nav-item">
-              <a className="nav-link active" aria-current="page" href="#about">
-                About
-              </a>
-            </li>
-            <li className="nav-item">
-              <a className="nav-link" href="#">
-                Projects
-              </a>
-            </li>
-            <li className="nav-item">
-              <a className="nav-link" href="#">
-                Contact Me
-              </a>
-            </li>
-          </ul>
-        </div>
-      </div>
-    </nav>
+    <Navbar bg="dark" variant="dark" expand="lg">
+      <Container>
+        <Navbar.Brand href="#">Shem Baijoo</Navbar.Brand>
+        <Navbar.Toggle aria-controls="basic-navbar-nav" />
+        <Navbar.Collapse id="basic-navbar-nav">
+          <Nav className="me-auto">
+            <Nav.Link href="#about">About Me</Nav.Link>
+            <Nav.Link href="#projects">Projects</Nav.Link>
+            <Nav.Link href="#contact">Contact Me</Nav.Link>
+          </Nav>
+        </Navbar.Collapse>
+      </Container>
+    </Navbar>
   );
 };
 
--- src/components/Projects.js
+++ src/components/Projects.js
@@ -1,86 +1,70 @@
 import React from "react";
+import { Card, Container } from "react-bootstrap";
+import img from "../assets/images/weather.PNG";
+import img2 from "../assets/images/password-image.PNG";
+import img3 from "../assets/images/planner.PNG";
 
 const Projects = () => {
   return (
-    <div
-      id="work"
-      className="carousel carousel-dark slide border border-info border-5"
-      data-bs-ride="carousel"
-    >
-      <h1>Work</h1>
-      <div className="carousel-indicators">
-        <button
-          type="button"
-          data-bs-target="#work"
-          data-bs-slide-to="0"
-          className="active"
-          aria-current="true"
-          aria-label="Slide 1"
-        ></button>
-        <button
-          type="button"
-          data-bs-target="#work"
-          data-bs-slide-to="1"
-          aria-label="Slide 2"
-        ></button>
-        <button
-          type="button"
-          data-bs-target="#work"
-          data-bs-slide-to="2"
-          aria-label="Slide 3"
-        ></button>
-      </div>
-      <div className="carousel-inner">
-        <div className="carousel-item active">
-          <a
+    <Container id="projects">
+      <h1>Projects</h1>
+      <Card style={{ width: "25rem", display: "inline" }}>
+        <Card.Img variant="top" fluid="true" src={img} />
+        <Card.Body>
+          <Card.Title>Weather Dashboard</Card.Title>
+          <Card.Link
+            href="https://github.com/spb71/weather-dashboard"
+            target="_blank"
+          >
+            Repo
+          </Card.Link>
+          <Card.Link
             href="https://spb71.github.io/weather-dashboard/"
             target="_blank"
-            rel="noreferrer"
           >
-            <img src="../assets/images/weather.PNG" alt="weather dashboard" />
-          </a>
-        </div>
-        <div className="carousel-item">
-          <a
+            Deployed Webpage
+          </Card.Link>
+        </Card.Body>
+      </Card>
+      <br />
+      <Card style={{ width: "25rem", display: "inline" }}>
+        <Card.Img variant="top" fluid="true" src={img2} />
+        <Card.Body>
+          <Card.Title>Password Authenticator</Card.Title>
+          <Card.Link
+            href="https://github.com/spb71/password-authentication"
+            target="_blank"
+          >
+            Repo
+          </Card.Link>
+          <Card.Link
             href="https://spb71.github.io/password-authentication/"
             target="_blank"
-            rel="noreferrer"
           >
-            <img
-              src="../assets/images/password-image.PNG"
-              alt="password authenticator"
-            />
-          </a>
-        </div>
-        <div className="carousel-item">
-          <a
+            Deployed Webpage
+          </Card.Link>
+        </Card.Body>
+      </Card>
+      <br />
+      <Card style={{ width: "25rem", display: "inline" }}>
+        <Card.Img variant="top" fluid="true" src={img3} />
+        <Card.Body>
+          <Card.Title>Daily Planner</Card.Title>
+          <Card.Link
+            href="https://github.com/spb71/daily-planner"
+            target="_blank"
+          >
+            Repo
+          </Card.Link>
+          <Card.Link
             href="https://spb71.github.io/daily-planner/"
             target="_blank"
-            rel="noreferrer"
           >
-            <img src="../assets/images/planner.PNG" alt="daily planner" />
-          </a>
-        </div>
-      </div>
-      <button
-        className="carousel-control-prev"
-        type="button"
-        data-bs-target="#work"
-        data-bs-slide="prev"
-      >
-        <span className="carousel-control-prev-icon" aria-hidden="true"></span>
-        <span className="visually-hidden">Previous</span>
-      </button>
-      <button
-        className="carousel-control-next"
-        type="button"
-        data-bs-target="#work"
-        data-bs-slide="next"
-      >
-        <span className="carousel-control-next-icon" aria-hidden="true"></span>
-        <span className="visually-hidden">Next</span>
-      </button>
-    </div>
+            Deployed Webpage
+          </Card.Link>
+        </Card.Body>
+      </Card>
+    </Container>
   );
 };
 
